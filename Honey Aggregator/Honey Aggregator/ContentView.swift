efdca408a0a9f187625ce895321555d5732ca6b8
matlockr <61714878+matlockr@users.noreply.github.com>
2021-01-20 17:03:22 -0800

    Made SwiftUI Project

---
+++ Honey Aggregator/Honey Aggregator/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Honey Aggregator
+//
+//  Created by user190078 on 1/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world")
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

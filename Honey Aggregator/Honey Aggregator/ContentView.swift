2c1ebc42accff6f753d032514f26d302eaf51057
matlockr <61714878+matlockr@users.noreply.github.com>
2021-01-21 15:56:23 -0800

    Testing Main Page UI

--- Honey Aggregator/Honey Aggregator/ContentView.swift
+++ Honey Aggregator/Honey Aggregator/ContentView.swift
@@ -9,14 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world")
-            .multilineTextAlignment(.center)
-            .padding()
+        
+        VStack {
+            Text("Honey Aggregator")
+                .fontWeight(.bold)
+                .foregroundColor(Color.orange)
+                .multilineTextAlignment(.center)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            Image("comb")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.all)
+            
+            Spacer()
+            
+            VStack{
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("New Hive")
+                        .font(.title)
+                        .bold()
+                        
+                }.padding()
+                .background(Color.orange)
+                
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Hive List")
+                        .font(.title)
+                        .bold()
+                        
+                }.padding()
+                .background(Color.orange)
+            }
+
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }

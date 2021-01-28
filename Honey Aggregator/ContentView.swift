e4794655bea66c0a5e76127dcf5bf782b3cea6a3
matlockr <61714878+matlockr@users.noreply.github.com>
2021-01-28 08:34:49 -0800

    Initial Commit

---
+++ Honey Aggregator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honey Aggregator
+//
+//  Created by user190078 on 1/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Honey Aggregator/Honey_AggregatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Honey_AggregatorApp.swift
+//  Honey Aggregator
+//
+//  Created by user190078 on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Honey_AggregatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

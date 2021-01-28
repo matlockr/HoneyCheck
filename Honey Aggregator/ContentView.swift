8dc86cf4fc788e95991c09dcee383278a6554aeb
matlockr <61714878+matlockr@users.noreply.github.com>
2021-01-28 08:59:22 -0800

    Fixing Xcode GitHub problems

--- Honey Aggregator/ContentView.swift
+++ Honey Aggregator/ContentView.swift
@@ -2,20 +2,56 @@
 //  ContentView.swift
 //  Honey Aggregator
 //
-//  Created by user190078 on 1/28/21.
+//  Created by user190078 on 1/20/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{
+            VStack {
+                Text("Honey\nAggregator")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.orange)
+                    .multilineTextAlignment(.center)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                
+                Spacer()
+                
+                Image("comb")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all)
+                
+                Spacer()
+                
+                VStack{
+                    NavigationLink(destination: HiveCreator()){
+                        Text("New Hive")
+                            .font(.title)
+                            .bold()
+                            .padding(.all)
+                        }.buttonStyle(PlainButtonStyle())
+                    NavigationLink(destination: HiveListUI()){
+                        Text("Hive List")
+                            .font(.title)
+                            .bold()
+                        }.buttonStyle(PlainButtonStyle())
+                }
+                .navigationBarTitle("", displayMode: .inline)
+                .foregroundColor(.orange)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }

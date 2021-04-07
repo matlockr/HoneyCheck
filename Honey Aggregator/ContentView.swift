a93b3ce65e3dcd501f365faadf9b188c6501972b
Robert Matlock <matlockr@sou.edu>
2021-04-07 08:57:31 -0700

    Removed Everything

---
+++ Honey Aggregator/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Honey Aggregator
+//
+//  Main Screen of the app
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("TEST")
+    }
+}

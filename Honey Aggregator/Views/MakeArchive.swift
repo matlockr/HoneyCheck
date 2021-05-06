36d6e28853babe3a338f794677e3fa36fd23d09e
Nick <moralesn@sou.edu>
2021-05-05 19:34:26 -0700

    Archiving added to the system, seems to be working.

---
+++ Honey Aggregator/Views/MakeArchive.swift
@@ -0,0 +1,52 @@
+//
+//  Make Archive.swift
+//  Honey Aggregator
+//
+//  Created by Nico Morales on 5/5/21.
+//
+
+import SwiftUI
+
+struct MakeArchive: View {
+    @EnvironmentObject var hives:Hives
+    @Environment(\.presentationMode) var presentationMode
+    @State var seasonName: String
+    @State var warning: String
+    var body: some View {
+        VStack( content: {
+            Spacer()
+            Text("Do you want to archive the season?  You can't edit it once it is archived.").padding()
+            Spacer()
+            TextField("Name this season", text: $seasonName).padding()
+            Button(action: {
+                let check = hives.archive(file: "\(seasonName).json")
+                if check == "Season was successfully archived!"{
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+                else{
+                    warning = check
+                }
+            }){
+                Text("Archive")
+            }.padding(20).disabled(seasonName.isEmpty)
+            if(warning.isEmpty){
+                Text("\(warning)").hidden()
+            }
+            else{
+                Text("\(warning)").padding()
+            }
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }){
+                Text("Cancel")
+            }.padding(20)
+        })
+        
+    }
+}
+/*
+struct Make_Archive_Previews: PreviewProvider {
+    static var previews: some View {
+        Make_Archive()
+    }
+}*/
---
+++ Honey Aggregator/Views/ResetHives.swift
@@ -0,0 +1,37 @@
+//
+//  ResetHives.swift
+//  Honey Aggregator
+//
+//  Created by Nico Morales on 5/5/21.
+//
+
+import SwiftUI
+
+struct ResetHives: View {
+    @EnvironmentObject var hives:Hives
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20, content: {
+            
+            Text("Confirm Reset?")
+            Button(action: {
+                hives.reset()
+                self.presentationMode.wrappedValue.dismiss()
+            }){
+                Text("Reset")
+            }
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }){
+                Text("Cancel")
+            }
+        })
+        
+    }
+}
+
+struct ResetHives_Previews: PreviewProvider {
+    static var previews: some View {
+        ResetHives()
+    }
+}
